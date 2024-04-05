9f12cba55555af4957edbb15cd7aaf4427fca607
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-04-05 18:34:51 -0300

    Add files via upload

---
+++ Atividade02sw/script.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function(e) {});
+
+function funcaobotaoUm(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+
+    alert("1");
+}
+
+function funcaobotaoDois(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoTres(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoQuatro(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoCinco(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoSeis(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoSete(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoOito(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotaoNove(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
+function funcaobotao(){
+    var botaoUm = document.querySelector("#botaoUm")
+    botaoUm.addEventListener("click", botaoUm)
+}
+
