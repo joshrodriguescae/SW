f5313bc6cc4ee7f30c53a3b05e23e164c95f52b7
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-04-26 18:16:08 -0300

    Add files via upload

---
+++ Aula05042024-SW/script.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", function(e) {});
+
+function funcaobotaoUm(){
+
+    alert("1");
+}
+
+function funcaobotaoDois(){
+
+    alert("2");
+}
+
+function funcaobotaoTres(){
+
+    alert("3");
+}
+
+function funcaobotaoQuatro(){
+
+    alert("4");
+}
+
+function funcaobotaoCinco(){
+
+    alert("5");
+}
+
+function funcaobotaoSeis(){
+
+    alert("6");
+}
+
+function funcaobotaoSete(){
+
+    alert("7");
+}
+
+function funcaobotaoOito(){
+
+    alert("8");
+}
+
+function funcaobotaoNove(){
+
+    alert("9");
+}
+
+function funcaobotaoZero(){
+
+    alert("0");
+}
+
+function funcaobotaoSoma(){
+
+    alert("+");
+}
+
+function funcaobotaoSubtracao(){
+
+    alert("-");
+}
+
+function funcaobotaoX(){
+
+    alert("X");
+}
+
+function funcaobotaoDivisao(){
+
+    alert("/");
+}
+
+function funcaobotaoC(){
+
+    alert("C");
+}
+
+function funcaobotaoCE(){
+
+    alert("CE");
+}
+
+function funcaobotaoVirgula(){
+
+    alert(",");
+}
+
+function funcaobotaoSeta(){
+
+    alert("⬅");
+}
+
+function funcaobotaoPorcentagem(){
+
+    alert("%");
+}
+
+function funcaobotaoResultado(){
+
+    alert("=");
+}
+
