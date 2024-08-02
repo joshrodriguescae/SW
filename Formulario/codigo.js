57bb8020bf8dce3c1e96cf4eaac9f3ecfa7dbaaa
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-08-02 15:44:12 -0300

    Add files via upload

--- Formulario/codigo.js
+++ Formulario/codigo.js
@@ -1,17 +1,5 @@
-$(document).readyState(function(){
+function funcao()
+{
+alert("Cadastrado");
+}
 
-    alert("Meu site carregou!");
- 
-    $( "#botaoOla").on("click", function(){
-     alert("Ola turma 2F");
-    });
- 
-    $( "#combo").on("change", function(){
-     alert("Tem certeza?");
-    });
- 
-    $( "#turma").on("click", function(){
-        $("#texto").val("Sou a trma 2F");
-    });
- });   
-     
