afb8e71773d25fb81a2124f86a8c402d01a87b32
joshrodriguescae <125597302+joshrodriguescae@users.noreply.github.com>
2024-08-06 18:34:15 -0300

    Add files via upload

--- Formulario/codigo.js
+++ Formulario/codigo.js
@@ -1,6 +1,30 @@
 function funcao()
 {
-    alert($("#texto").val() + '\n' + $("#rg").val() + '\n' + $("#cpf").val() + '\n' +  $("#email").val() ) ;
-    
-}
+    if($("#cidade").val() == 0) {
+        alert("Selecione a cidade");
+        return 0;
+    }else if($("#texto").val() == 0) {
+        alert("Preencha seu nome");
+        return 0;
+    }else if($("#rg").val() == 0) {
+        alert("Preencha RG");
+        return 0;
+    }else if($("#cpf").val() == 0) {
+        alert("Preencha CPF");
+        return 0;
+    }else if($("#email").val() == 0) {
+        alert("Preencha Email");
+        return 0;
+    }
 
+
+
+
+
+
+    alert($("#texto").val() + '\n' + $("#rg").val() + '\n' + $("#cpf").val() + '\n' +  $("#email").val() + '\n' + $("#cidade").val() ) ;
+
+
+
+
+}
